abca0d1f61191c40a3276951c570c0703c8dfd75
sena <hargiantofebrisena@gmail.com>
2022-08-10 00:11:27 +0700

    initial

--- src/socket/index.js
+++ src/socket/index.js
@@ -4,7 +4,8 @@ const connectedUser = {};
 const socketIo = (io) => {
   io.on("connection", (socket) => {
     console.log("client connect:", socket.id);
-    socket.on("load admin contact", async () => {
+
+    socket.on("load order", async () => {
       try {
         let datas = await Order.findAll();
         socket.emit("orders", datas);
@@ -13,9 +14,17 @@ const socketIo = (io) => {
       }
     });
 
+    socket.on("send order", async (payload) => {
+      try {
+        console.log("socket id " + socket.id);
+        io.to(socket.id).emit("new order", idRecipient);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
     socket.on("disconnect", () => {
       console.log("client disconnect");
-      delete connectedUser[userId];
     });
   });
 };
