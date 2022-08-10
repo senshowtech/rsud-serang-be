36381f1f1aff14c3abf4cc58d6bd49d043f0c258
sena <hargiantofebrisena@gmail.com>
2022-08-10 15:35:46 +0700

    initial

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,22 +1,27 @@
 const { Order } = require("../../models");
 
+const connectedUser = {};
 const socketIo = (io) => {
   io.on("connection", (socket) => {
     console.log("client connect:", socket.id);
+    const userId = socket.handshake.query.id;
+    connectedUser[userId] = socket.id;
 
-    socket.on("load order", async () => {
+    socket.on("load messages", async (payload) => {
       try {
-        let datas = await Order.findAll();
-        socket.emit("orders", datas);
-      } catch (err) {
-        console.log(err);
+        const data = await Order.findAll();
+        socket.emit("messages", data);
+      } catch (error) {
+        console.log(error);
       }
     });
 
-    socket.on("send order", async () => {
+    socket.on("send messages", async (payload) => {
       try {
+        const { firstName, lastName, number, address } = payload;
+        await Order.create({ firstName, lastName, number, address });
         console.log("socket id " + socket.id);
-        io.to(socket.id).emit("new order");
+        io.to(socket.id).emit("new message");
       } catch (error) {
         console.log(error);
       }
@@ -24,6 +29,7 @@ const socketIo = (io) => {
 
     socket.on("disconnect", () => {
       console.log("client disconnect");
+      delete connectedUser[userId];
     });
   });
 };
